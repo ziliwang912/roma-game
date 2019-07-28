9cccb9b88169e9b8edfc466be530f774df65c9d8
ziliwang912 <ziliwang912@gmail.com>
2019-07-28 12:22:11 -0400

    change layout

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const choices = ["Lapis", "Papyrus", "Scalpellus"];
 const winners = ["Tie", "You", "Computer"];
 let userChoice;
 let computereChoice;
-let gameNumber = 0; // number of games played
-let numberOfWins = 0; // number of wins so far
+let numOfGames = 0; 
+let numOfWins = 0; 
+let numOfTies = 0; 
 
 
 // if Lapis is chosen
@@ -35,8 +36,9 @@ function computerChooses() {
 
 function addResult (gameNumber, choice1, choice2, winner) {
     const resultTable = document.querySelector("table");
-    if (resultTable.row[0].cell[0] == 0) {
-        // delete row 0;
+
+    if (resultTable.rows[1].cells[0].innerText == 0) {
+        resultTable.deleteRow(1); 
     } else {
         let row = resultTable.insertRow(-1);
         row.insertCell(0).innerText = gameNumber;
@@ -44,7 +46,6 @@ function addResult (gameNumber, choice1, choice2, winner) {
         row.insertCell(2).innerText = choice2;
         row.insertCell(3).innerText = winner;
     }
-
 }
 
 // compare two choices and show the result
@@ -52,47 +53,50 @@ function compareChoices(choice1, choice2) {
     if (choice1 === "Lapis") {
         if (choice2 === "Lapis") {
             alert ("Lapis Vs. Lapis! It's a tie!");
-            addResult(++gameNumber,choice1,choice2,winners[0]);
+            numOfTies++;
+            addResult(++numOfGames,choice1,choice2,winners[0]);
         } else if (choice2 === "Papyrus") {
             alert ("Lapis Vs. Papyrus! Computer wins!");
-            addResult(++gameNumber,choice1,choice2,winners[2]);
+            addResult(++numOfGames,choice1,choice2,winners[2]);
         } else {
             alert ("Lapis Vs. Scalpellus! You win!");
-            numberOfWins++;
-            addResult(++gameNumber,choice1,choice2,winners[1]);
+            numOfWins++;
+            addResult(++numOfGames,choice1,choice2,winners[1]);
         }
     } else if (choice1 === "Papyrus") {
         if (choice2 === "Lapis") {
             alert ("Papyrus Vs. Lapis! You win!");
-            addResult(++gameNumber,choice1,choice2,winners[1]);
-            numberOfWins++;
+            addResult(++numOfGames,choice1,choice2,winners[1]);
+            numOfWins++;
         } else if (choice2 === "Papyrus") {
             alert ("Papyrus Vs. Papyrus! It's a tie!");
-            addResult(++gameNumber,choice1,choice2,winners[0]);
+            numOfTies++;
+            addResult(++numOfGames,choice1,choice2,winners[0]);
         } else {
             alert ("Papyrus Vs. Scalpellus! Computer wins!");
-            addResult(++gameNumber,choice1,choice2,winners[2]);
+            addResult(++numOfGames,choice1,choice2,winners[2]);
         }
     } else {
         if (choice2 === "Lapis") {
             alert ("Scalpellus Vs. Lapis! Computer wins!");
-            addResult(++gameNumber,choice1,choice2,winners[2]);
+            addResult(++numOfGames,choice1,choice2,winners[2]);
         } else if (choice2 === "Papyrus") {
             alert ("Scalpellus Vs. Papyrus! You win!");
-            numberOfWins++;
-            addResult(++gameNumber,choice1,choice2,winners[1]);
+            numOfWins++;
+            addResult(++numOfGames,choice1,choice2,winners[1]);
         } else {
             alert ("Scalpellus Vs. Scalpellus! It's a tie!");
-            addResult(++gameNumber,choice1,choice2,winners[0]);
+            numOfTies++;
+            addResult(++numOfGames,choice1,choice2,winners[0]);
         }
     }
 };
 
 document.querySelector('#win_rate').onclick = function () {
-    if (gameNumber == 0) {
+    if (numOfGames == 0) {
         alert ("You haven't play any game yet!");
     } else {
-        let winRate = numberOfWins / gameNumber * 100;
-        alert ("Your win rate against our AI is " + winRate.toFixed(2) + "%.");
+        let winRate = numOfWins / (numOfGames - numOfTies) * 100;
+        alert ("Ignoring ties, Your win rate against computer is " + winRate.toFixed(2) + "%.");
     }
 };
